311ca007ab2d618f0b95cfa2f5070ae37fdb3e14
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-05-28 14:56:30 -0300

    calculoIMC 1.0

--- scrypt.js
+++ scrypt.js
@@ -1,9 +1,9 @@
 const botaoCalcular = document.getElementById('btnCalcular');
 
 function calculoIMC() {
-    let peso = document.getElementById("peso").value;
-    let altura = document.getElementById("altura").value / 100;
-    let resultado = document.getElementById("resultado");
+    const peso = document.getElementById("peso").value;
+    const altura = document.getElementById("altura").value / 100;
+    const resultado = document.getElementById("resultado");
 
     if (altura !== "" && peso !== "") {
         let imc = (peso / (altura * altura)).toFixed(1);
@@ -12,21 +12,41 @@ function calculoIMC() {
 
         if (imc <= 18.5) {
             mensagem = "Você está abaixo do peso ideal!"
+            resultado.classList.add("resultado-yellow")
         } else if (imc <= 25) {
             mensagem = "Você está com o peso ideal!"
+            resultado.classList.add("resultado-green")
         } else if (imc <= 30) {
             mensagem = "Você está acima do peso ideal!"
+            resultado.classList.add("resultado-yellow")
         } else if (imc <= 35) {
             mensagem = "Cuidado! Você está com obesidade grau I"
+            resultado.classList.add("resultado-red")
         } else if (imc <= 40) {
             mensagem = "Cuidado! Você está com obesidade grau II"
+            resultado.classList.add("resultado-red")
         } else {
             mensagem = "Cuidado! Você está com obesidade grau III"
+            resultado.classList.add("resultado-red")
         }
 
         resultado.textContent = `Seu IMC é: ${imc}. ${mensagem}`;
 
     } else {
         resultado.textContent = "Por favor, preencha todos os campos!"
+        resultado.classList.add("resultado-red")
     }
 }
+
+const reset = document.getElementById("reset");
+
+function clear() {
+    document.getElementById("peso").value = "";
+    document.getElementById("altura").value = "";
+    resultado.classList.remove("resultado-red");
+    resultado.classList.remove("resultado-green");
+    resultado.classList.remove("resultado-yellow");
+    resultado.textContent = "";
+}
+
+reset.addEventListener('click', clear());
